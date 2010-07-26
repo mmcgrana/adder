88ce237f8c8ba4d4230550a5288b62aa43ea8f12
Mark McGranaghan <mmcgrana@gmail.com>
2010-07-26 06:20:02 -0700

    depend on lein-run

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
      [ring/ring-devel "0.2.5"]
      [ring/ring-jetty-adapter "0.2.5"]
      [compojure "0.4.0"]
-     [hiccup "0.2.6"]])
+     [hiccup "0.2.6"]]
+  :dev-dependencies
+    [[lein-run "1.0.0-SNAPSHOT"]])
